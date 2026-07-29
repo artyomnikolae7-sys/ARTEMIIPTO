16d65cab330105dbe92e320ac259ceb77869d9de
artyomnikolae7-sys <artyomnikolae7@gmail.com>
2026-07-29 11:27:45 +0300

    Enhance Leaflet map with theme-adaptive tiles

--- app/app.tsx
+++ app/app.tsx
@@ -363,7 +363,11 @@ export default function App() {
       position: 'topright'
     }).addTo(map)
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    const tileUrl = theme === 'dark'
+      ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+      : 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+
+    L.tileLayer(tileUrl, {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 20
